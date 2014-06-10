4a7c515c10397e8a108a9eeafcca9be13ea0d86e
SALA <SALA@acwin7c124-2.uptc.edu.co>
2014-06-10 15:59:20 -0500

    Nueva Carpeta de files, y codigo en persistencia para manejar los archivos del karaoke

--- Karaoke_L_E/src/persistencia/GestionArchivoBinario.java
+++ Karaoke_L_E/src/persistencia/GestionArchivoBinario.java
@@ -1,8 +1,37 @@
 package persistencia;
 
+import java.io.*;
+
+import logica.karaoke;
+
 public class GestionArchivoBinario {
 
+
+	private ObjectInputStream objectInputStream;
+	private ObjectOutputStream objectOutputStream;
+	private File file;
+	
+	private FileInputStream fileInputStream;
+	private FileOutputStream fileOutputStream;
+	
+	
 	public GestionArchivoBinario() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void leer() throws IOException, ClassNotFoundException {
+		file = new File("./files/person");
+		fileInputStream = new FileInputStream(file);
+		objectInputStream = new ObjectInputStream(fileInputStream);
+		
+		karaoke karaoke  = (karaoke)objectInputStream.readObject();
+	}
+	public void  escribir()  throws IOException {
+		file = new File("./files/person");
+		fileOutputStream = new FileOutputStream(file);
+		objectOutputStream = new ObjectOutputStream(fileOutputStream);
+		karaoke karaoke = new karaoke();
+		objectOutputStream.writeObject(karaoke);//Guardar el objeto
+		objectOutputStream.close();
 	}
 }
--- Karaoke_L_E/src/persistencia/GestionArchivoPlano.java
+++ Karaoke_L_E/src/persistencia/GestionArchivoPlano.java
@@ -1,5 +1,43 @@
 package persistencia;
 
-public class GestionArchivoPlano {
+import java.io.*;
 
+import javax.swing.JFileChooser;
+
+public class GestionArchivoPlano {
+	private File file;
+	private FileWriter fileWriter;
+	private BufferedWriter bufferedWriter;
+
+	private FileReader fileReader;
+	private BufferedReader bufferedReader;
+	public void crearArchivoPlano() {
+		JFileChooser fileChooser =  new JFileChooser("src/files");
+		if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			file = new File(fileChooser.getSelectedFile().getPath());
+		}
+	}
+	public void leerPlano() throws IOException{
+		JFileChooser fileChooser =  new JFileChooser("src/files");
+		if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			file = new File(fileChooser.getSelectedFile().getPath());
+		}
+		fileReader = new FileReader(file);
+		while (fileReader.read() != -1) {
+			System.out.println((char)fileReader.read());
+		}
+	}
+	public void escribirArchivoPlano() throws IOException {
+		fileWriter = new FileWriter(file);
+		bufferedWriter = new BufferedWriter(fileWriter);
+		bufferedWriter.write("");
+		bufferedWriter.close();
+	}
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
 }
