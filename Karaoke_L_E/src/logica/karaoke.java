37fac98dbd13e2abaa97b7f66d1ed8d877200ef7
lina pc <lina pc@lina.uptc.edu.co>
2014-06-10 15:00:00 -0500

    Se crearon los atributos de las clases de logica.

--- Karaoke_L_E/src/logica/karaoke.java
+++ Karaoke_L_E/src/logica/karaoke.java
@@ -7,6 +7,28 @@ public class karaoke {
 	private ArrayList<Genero> listaGeneros;
 	private Cancion cancionActual;
 	
+	public karaoke(ArrayList<Genero> listaGeneros, Cancion cancionActual) {
+		super();
+		this.listaGeneros = listaGeneros;
+		this.cancionActual = cancionActual;
+	}
+
+	public karaoke() {
+	}
+	
+	public ArrayList<Genero> getListaGeneros() {
+		return listaGeneros;
+	}
+	public void setListaGeneros(ArrayList<Genero> listaGeneros) {
+		this.listaGeneros = listaGeneros;
+	}
+	public Cancion getCancionActual() {
+		return cancionActual;
+	}
+	public void setCancionActual(Cancion cancionActual) {
+		this.cancionActual = cancionActual;
+	}
+	
 	
 
 }
