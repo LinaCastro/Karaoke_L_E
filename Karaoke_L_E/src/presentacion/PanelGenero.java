ad7cee5e8f4d9edae3cb3ad81b703966c6fc6e0e
Edison < titojesct@gmail.com>
2014-06-12 15:07:00 -0500

    Test PanelGenero

--- Karaoke_L_E/src/presentacion/PanelGenero.java
+++ Karaoke_L_E/src/presentacion/PanelGenero.java
@@ -2,9 +2,14 @@ package presentacion;
 
 import javax.swing.JPanel;
 
+import logica.Genero;
+
 public class PanelGenero extends JPanel{
 
+	private Genero genero;
 	public PanelGenero() {
 		// TODO Auto-generated constructor stub
+	}
+	public PanelGenero(Genero generoP) {
 	}
 }
