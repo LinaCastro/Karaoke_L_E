aad34311bc00da0b903e4aecdf08107b6f5a3e5e
SALA <SALA@acwin7c126-1.uptc.edu.co>
2014-06-12 14:40:07 -0500

    Nuevos Atributos de la clase VentanaPrincipal

--- Karaoke_L_E/src/presentacion/VentanaPrincipal.java
+++ Karaoke_L_E/src/presentacion/VentanaPrincipal.java
@@ -1,10 +1,38 @@
 package presentacion;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Properties;
+
 import javax.swing.JFrame;
 
 public class VentanaPrincipal extends JFrame{
 
+	private PanelReproduccion panelReproduccion;
+	private PanelGenero panelGenero;
+	private PanelArtista panelArtista;
+	private PanelCancion panelCancion;
+	
+	
 	public VentanaPrincipal() {
-		// TODO Auto-generated constructor stub
+		super("Karaoke");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+		this.setLayout(new BorderLayout());
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setBackground(Color.decode("#51C1F5"));
+		this.setUndecorated(true);
+		this.setFocusable(true);
+		
+		panelReproduccion = new PanelReproduccion();
+		panelCancion = new PanelCancion();
+		panelGenero = new PanelGenero();
+		panelArtista = new PanelArtista();
+		
+		this.add(panelReproduccion);
+		this.add(panelCancion);
+		this.add(panelGenero);
+		this.add(panelArtista);
+
 	}
 }
